20d37da9cc76b42ee8f95f43338e3252892d5261
humanova <craxes_humanova@hotmail.com>
2018-07-18 15:25:26 +0300

    v0.2.6 (!kripto gorunum)
    yazi duzetlmesi

--- hmnBot.py
+++ hmnBot.py
@@ -337,7 +337,7 @@ async def on_message(message):
                     if not str(kur_degisim).startswith("-"):
                         embed.add_field(name="Günlük Değişim",value=":arrow_up_small: " + str(kur_degisim) + "%", inline=True)
                     else:
-                        embed.add_field(name="Günlük Değişim",value=":arrow_down_small: % " + str(kur_degisim) + "%", inline=True)
+                        embed.add_field(name="Günlük Değişim",value=":arrow_down_small: " + str(kur_degisim) + "%", inline=True)
 
                     embed.add_field(name="Son 7 günlük grafik", value=yazi.komut["kripto-cizgi"], inline=True)
                     embed.set_image(url=grafik_link)

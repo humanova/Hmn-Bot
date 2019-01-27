9a7380b5ac50f3bdab49ea93ea43aac8d24fb932
humanova <craxes_humanova@hotmail.com>
2019-01-27 22:23:24 +0300

    eval fix

--- src/botEval.py
+++ src/botEval.py
@@ -5,12 +5,11 @@ import os
 import subprocess
 
 def EvalHmnBot(commands):
-    commands = " ".join(commands[:])
     p = subprocess.Popen(commands, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
     
     for line in p.stdout.readlines():
         print(line)
-        
+
     return_val = p.wait()
 
     return return_val

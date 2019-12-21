75c7e5f329011dfbfce7683ce0aa32d742dcddd4
humanova <craxes_humanova@hotmail.com>
2019-12-21 23:11:36 +0300

    moved help command override to bot.py

--- bot/bot.py
+++ bot/bot.py
@@ -1,20 +1,69 @@
 # 2019 Emir Erbasan (humanova)
 # MIT License, see LICENSE for more details
 
-from discord.ext import commands
-from discord.ext.commands import AutoShardedBot, DefaultHelpCommand
 import sys
 import traceback
+from discord.ext import commands
+from discord.ext.commands import AutoShardedBot, DefaultHelpCommand
 from utils import permissions
 from datetime import datetime
 
-init_extensions = ['cogs.utility', 'cogs.owner', 'cogs.info', 'cogs.admin', 'cogs.fun', 'cogs.meme', 'cogs.currency']
+init_extensions = ['cogs.utility',
+                   'cogs.owner',
+                   'cogs.info',
+                   'cogs.admin',
+                   'cogs.fun',
+                   'cogs.meme',
+                   'cogs.currency',
+                   'cogs.weather']
 
+help_cmd_attrs = {'name': 'yardım',
+                 'help': 'Bu mesajı gösterir.'}
+
+
+# override !help command
+class HelpFormat(DefaultHelpCommand):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, command_attrs=help_cmd_attrs, **kwargs)
+
+    def get_destination(self, no_pm: bool = False):
+        if no_pm:
+            return self.context.channel
+        else:
+            return self.context.author
+
+    async def send_error_message(self, error):
+        destination = self.get_destination(no_pm=True)
+        await destination.send(error)
+
+    async def send_command_help(self, command):
+        self.add_command_formatting(command)
+        self.paginator.close_page()
+        await self.send_pages(no_pm=True)
+
+    def get_ending_note(self):
+        return "!yardım <komut> yazarak bir komut hakkında daha fazla bilgi al."
+
+    async def send_pages(self, no_pm: bool = False):
+        try:
+            if permissions.can_react(self.context):
+                await self.context.message.add_reaction(chr(0x2709))
+        except discord.Forbidden:
+            pass
+
+        try:
+            destination = self.get_destination(no_pm=no_pm)
+            for page in self.paginator.pages:
+                await destination.send(page)
+        except discord.Forbidden:
+            destination = self.get_destination(no_pm=True)
+            for page in self.paginator.pages:
+                await destination.send(page)
 
 
 class Bot(AutoShardedBot):
     def __init__(self, *args, prefix=None, **kwargs):
-        super().__init__(*args, **kwargs)
+        super().__init__(*args, help_command=HelpFormat(), **kwargs)
 
         for ext in init_extensions:
             self.load_extension(ext)

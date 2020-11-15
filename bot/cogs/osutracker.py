02ef36bb5822f6ea297db59f8014a02a9cbbc183
humanova <craxes_humanova@hotmail.com>
2020-11-15 18:55:37 +0300

    fix osutracker cog

--- bot/cogs/osutracker.py
+++ bot/cogs/osutracker.py
@@ -16,9 +16,8 @@ class OsuTracker(commands.Cog):
         self.api = OsuApi(key=self.config.osu_token, connector=ReqConnector())
         self.last_check_datetime = datetime.datetime.utcnow()
         self.tracking_channel = None
+        self.is_started = False
         self.tracked_users = []
-
-        self.tracker_task_loop.start()
 
     def cog_unload(self):
         self.tracker_task_loop.cancel()
@@ -29,16 +28,16 @@ class OsuTracker(commands.Cog):
             scores = self.api.get_user_best(user['name'], limit=30)
             for s in [s for s in scores if s.date > self.last_check_datetime]:
                 bmap = self.api.get_beatmaps(beatmap_id=s.beatmap_id)[0]
-                msg = f"{user['nick']} abım {bmap.title} - {bmap.version} mapınde {int(round(s.pp))} pp play\
-                 yapmıs helal abım <@{user['discord'].id}>\n".lower().replace('i', 'ı')
-                self.tracking_channel.send(msg)
+                msg = f"{user['nick']} abım {bmap.title} - {bmap.version} mapınde {int(round(s.pp))} pp play yapmıs\n" \
+                      f"helal abım <@{user['discord'].id}>\n".lower().replace('i', 'ı')
+                await self.tracking_channel.send(msg)
         self.last_check_datetime = datetime.datetime.utcnow()
 
     @commands.command()
     @commands.check(permissions.is_owner)
     async def osutrack(self, ctx, mem: discord.Member, osu_username, nick):
         if osu_username not in [u['name'] for u in self.tracked_users]:
-            self.tracked_users.append({"name": osu_username, nick: nick, discord: mem})
+            self.tracked_users.append({"name": osu_username, "nick": nick, "discord": mem})
             await ctx.send(f"tracking started : {str(mem)}")
         else:
             await ctx.send(f"already tracking : {str(mem)}")
@@ -46,9 +45,13 @@ class OsuTracker(commands.Cog):
     @commands.command()
     @commands.check(permissions.is_owner)
     async def startosutracker(self, ctx):
-        self.tracking_channel = ctx
-        await ctx.send(f"ok")
-
+        if not self.is_started:
+            self.tracking_channel = ctx
+            self.tracker_task_loop.start()
+            self.is_started = True
+            await ctx.send(f"ok")
+        else:
+            await ctx.send(f"tracker already started")
 
 def setup(bot):
     bot.add_cog(OsuTracker(bot))
